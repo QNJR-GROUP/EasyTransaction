1b9d188bbcd28cfa795b913a0a83f58127cdf3fe
FJHHH <1375006685@qq.com>
2020-09-22 23:39:18 +0800

    OnsEasyTransMsgConsumerImpl 中对 topicSubs 排序， 防止顺序不一致导致 ons 出现订阅关系不一致

--- easytrans-queue-ons-starter/src/main/java/com/yiqiniu/easytrans/queue/impl/ons/OnsEasyTransMsgConsumerImpl.java
+++ easytrans-queue-ons-starter/src/main/java/com/yiqiniu/easytrans/queue/impl/ons/OnsEasyTransMsgConsumerImpl.java
@@ -1,9 +1,6 @@
 package com.yiqiniu.easytrans.queue.impl.ons;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 import com.aliyun.openservices.ons.api.Action;
 import com.aliyun.openservices.ons.api.ConsumeContext;
@@ -36,7 +33,11 @@ public class OnsEasyTransMsgConsumerImpl implements EasyTransMsgConsumer {
 	
 	private String getAliTagsString(Collection<String> topicSubs) {
 		StringBuilder sb = new StringBuilder();
-		for(String s:topicSubs){
+
+		List<String> topicSubList = new ArrayList<>(topicSubs);
+		Collections.sort(topicSubList);
+
+		for(String s : topicSubList) {
 			sb.append(s);
 			sb.append("||");
 		}
